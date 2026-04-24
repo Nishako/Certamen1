113ce827007f67b4681f411f0cb40f2408c6ca1d
Mishas Silva <mishasabdiel@gmail.com>
2026-04-24 15:08:34 -0400

    Commit inicial: Configuración de la API SansaLibros

---
+++ src/test/java/com/sansalibros/api/SansalibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sansalibros.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SansalibrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
